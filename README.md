de8a9f18a323e46367d08125f06697f4d7312ec8
ebouther <ebouther@student.42.fr>
2022-06-09 09:20:09 +0200

    chore: netboard translations

--- README.md
+++
@@ -1,2 +0,0 @@
-# WPRN
-Societal impacts and aftermath of Covid-19: A global searchable directory
---
+++ bo_translations/languages.js
@@ -0,0 +1,16 @@
+export default [
+  {
+    code: 'fr',
+    iso: 'fr-FR',
+    name: 'French',
+    file: 'fr.json',
+    dir: 'ltr',
+  },
+  {
+    code: 'en',
+    iso: 'en-US',
+    name: 'English',
+    file: 'en.json',
+    dir: 'ltr',
+  },
+]
